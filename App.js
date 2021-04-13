780060408959403bc251b3a906148528395b9d07
ajakcyer <ajakcyer97@gmail.com>
2021-04-13 00:27:56 -0400

    by default every view organizes it's children with flexbox

--- App.js
+++ App.js
@@ -10,17 +10,54 @@ import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 export default function App() {
 
   return (
-    <View style={{padding: 30}}>
+    // <View style={{padding: 30}}>
       
-      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+    //   <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
         
-        <TextInput placeholder="Course Goal" style={{borderColor: 'black', borderWidth: 1, padding: 10, width: '80%'}}/>
-        <Button title="Add"/>
-      </View>
-      <View>
+    //     <TextInput placeholder="Course Goal" style={{borderColor: 'black', borderWidth: 1, padding: 10, width: '80%'}}/>
+    //     <Button title="Add"/>
+    //   </View>
+    //   <View>
 
+    //   </View>
+    // </View>
+
+    <View style={{padding: 50}}>
+      <View
+      style={{
+        backgroundColor: 'red',
+        width: 100,
+        height: 100,
+        justifyContent: 'center',
+        alignItems: 'center'}}>
+        <Text>1</Text>
       </View>
+
+      <View
+      style={{
+        backgroundColor: 'blue',
+        width: 100,
+        height: 100,
+        justifyContent: 'center',
+        alignItems: 'center'}}>
+        <Text>2</Text>
+      </View>
+
+      <View
+      style={{
+        backgroundColor: 'green',
+        width: 100,
+        height: 100,
+        justifyContent: 'center',
+        alignItems: 'center'}}>
+        <Text>3</Text>
+      </View>
+
     </View>
+
+
+
+
   );
 }
 
