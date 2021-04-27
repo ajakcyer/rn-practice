62c724ce6af884d858a6574abe50cb62713b9c49
ajakcyer <ajakcyer97@gmail.com>
2021-04-26 22:00:56 -0400

    create stylesheet object

--- App.js
+++ App.js
@@ -10,52 +10,52 @@ import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 export default function App() {
 
   return (
-    // <View style={{padding: 30}}>
+    <View style={{padding: 30}}>
       
-    //   <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
         
-    //     <TextInput placeholder="Course Goal" style={{borderColor: 'black', borderWidth: 1, padding: 10, width: '80%'}}/>
-    //     <Button title="Add"/>
-    //   </View>
-    //   <View>
-
-    //   </View>
-    // </View>
-
-    <View style={{padding: 50, flexDirection: 'row', width: '80%', height: 300, justifyContent: 'space-around', alignItems: 'stretch'}}>
-      <View
-      style={{
-        backgroundColor: 'red',
-        flex: 1,
-        // width: 100,
-        // height: 100,
-        justifyContent: 'center',
-        alignItems: 'center'}}>
-        <Text>1</Text>
+        <TextInput placeholder="Course Goal" style={{borderColor: 'black', borderWidth: 1, padding: 10, width: '80%'}}/>
+        <Button title="Add"/>
       </View>
+      <View>
 
-      <View
-      style={{
-        backgroundColor: 'blue',
-        // width: 100,
-        // height: 100,
-        flex: 2,
-        justifyContent: 'center',
-        alignItems: 'center'}}>
-        <Text>2</Text>
       </View>
-
-      <View
-      style={{
-        backgroundColor: 'green',
-        // width: 100,
-        // height: 100,
-        justifyContent: 'center',
-        alignItems: 'center'}}>
-        <Text>3</Text>
-      </View>
-
     </View>
+
+    // <View style={{padding: 50, flexDirection: 'row', width: '80%', height: 300, justifyContent: 'space-around', alignItems: 'stretch'}}>
+    //   <View
+    //   style={{
+    //     backgroundColor: 'red',
+    //     flex: 1,
+    //     // width: 100,
+    //     // height: 100,
+    //     justifyContent: 'center',
+    //     alignItems: 'center'}}>
+    //     <Text>1</Text>
+    //   </View>
+
+    //   <View
+    //   style={{
+    //     backgroundColor: 'blue',
+    //     // width: 100,
+    //     // height: 100,
+    //     flex: 2,
+    //     justifyContent: 'center',
+    //     alignItems: 'center'}}>
+    //     <Text>2</Text>
+    //   </View>
+
+    //   <View
+    //   style={{
+    //     backgroundColor: 'green',
+    //     // width: 100,
+    //     // height: 100,
+    //     justifyContent: 'center',
+    //     alignItems: 'center'}}>
+    //     <Text>3</Text>
+    //   </View>
+
+    // </View>
 
 
 
@@ -71,3 +71,7 @@ export default function App() {
 //     justifyContent: "center",
 //   },
 // });
+
+const styles = StyleSheet.create({
+  
+})
